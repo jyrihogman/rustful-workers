b4c413291db61fd9f6c06b5cbaed9bbc0575fcb4
raitse <tommi.raitanen@gmail.com>
2024-03-16 19:16:07 +0200

    Add turso-aws-lambda module (#3)
    * Add turso-aws-lambda module
    
    Fetch users from Turso DB users table.
    
    * Remove unneeded dotenv depedency
    
    * Fix intendation in file

---
+++ turso-aws-lambda/src/db.rs
@@ -0,0 +1,26 @@
+use libsql::{de, Builder};
+use serde::Serialize;
+
+use std::env;
+
+#[derive(Debug, serde::Deserialize, Serialize)]
+#[allow(dead_code)]
+pub struct User {
+    id: String,
+    id_hash: String,
+    display_name: String,
+}
+
+pub async fn get_messages() -> Result<Vec<User>, libsql::Error> {
+    let url = env::var("LIBSQL_URL").expect("DB_URL env variable not set");
+    let auth_token = env::var("LIBSQL_AUTH_TOKEN").expect("DB_TOKEN env variable not set");
+
+    let db = Builder::new_remote(url, auth_token).build().await.unwrap();
+    let mut rows = db.connect()?.query("SELECT * FROM users", ()).await?;
+    let mut users = Vec::new();
+    while let Some(row) = rows.next().await.unwrap() {
+        let user = de::from_row::<User>(&row).unwrap();
+        users.push(user);
+    }
+    Ok(users)
+}
